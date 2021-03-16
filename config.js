429c8a047422528342b12fe7ef232475cb37f570
Stackbit Code Editor <projects@stackbit.com>
2021-03-16 13:50:09 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
+    spotify: 'https://ryanfitzgerald.ca',
     github: 'https://github.com/ryanfitzgerald',
     twitter: 'https://twitter.com/ryanafitzgerald',
     linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
