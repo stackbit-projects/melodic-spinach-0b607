5a6f7a2474425761ecc4ad105b855f918c36e6a6
Stackbit Code Editor <projects@stackbit.com>
2021-03-16 13:50:40 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   postsPerPage: 5, // Required
   social: {
     spotify: 'https://#',
-    twitter: 'https://twitter.com/ryanafitzgerald',
+    acerca: 'https://twitter.com/ryanafitzgerald',
     linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
   },
   pathPrefix: '/blog',
